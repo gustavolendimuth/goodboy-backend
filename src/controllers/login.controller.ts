90cb4f0375ce5f97fc5edf1a8d218f32cc86d58a
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-12-20 22:50:38 -0300

    added cors to login route

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import loginService from '../services/login.service';
 
 const login = async (req: Request, res: Response) => {
+  res.set('Access-Control-Allow-Origin', '*');
   const { body } = req;
   const response = await loginService(body);
 
