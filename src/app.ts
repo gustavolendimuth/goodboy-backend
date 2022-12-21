ad5166005aadc9049f7e36a4e84fbba1d0d10119
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-12-20 23:55:55 -0300

    added test route

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import loginRouter from './routes/login.route';
 import userRouter from './routes/user.router';
-import authMiddleware from './middlewares/auth.middleware';
+// import authMiddleware from './middlewares/auth.middleware';
 import orderRouter from './routes/order.route';
 import ordersRouter from './routes/orders.route';
 import cors from 'cors';
@@ -22,10 +22,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/process_payment', processPaymentRouter);
-app.use(authMiddleware)
+// app.use(authMiddleware)
 app.use('/order', orderRouter);
 app.use('/orders', ordersRouter);
 
