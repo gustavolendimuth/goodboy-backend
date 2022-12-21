80a45e5f56db303db86ad957c92fbb9767edbc5d
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2022-12-21 12:59:50 -0300

    added frontend url variable

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,26 @@ import express, { Request, Response } from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import loginRouter from './routes/login.route';
 import userRouter from './routes/user.router';
-// import authMiddleware from './middlewares/auth.middleware';
+import authMiddleware from './middlewares/auth.middleware';
 import orderRouter from './routes/order.route';
 import ordersRouter from './routes/orders.route';
 import cors from 'cors';
 import processPaymentRouter from './routes/process.payment.route';
 
-// const corsOptions = {
-//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//   optionsSuccessStatus: 200
-// }
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req:Request, res:Response) => res.send('Hello World!'));
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/process_payment', processPaymentRouter);
-// app.use(authMiddleware)
+app.use(authMiddleware)
 app.use('/order', orderRouter);
 app.use('/orders', ordersRouter);
 
--- src/utils/sendMagicLink.ts
+++ src/utils/sendMagicLink.ts
@@ -1,25 +1,30 @@
 import nodemailer from 'nodemailer';
 
-const host = process.env.EMAIL_HOST;
+const host = process.env.EMAIL_HOST || 'smtp.gmail.com';
+const port = Number(process.env.EMAIL_PORT) || 465;
+const secure = Boolean(process.env.EMAIL_SECURE) || false;
+const user = process.env.EMAIL_USER;
+const pass = process.env.EMAIL_PASS;
 
 if (!host) throw new Error('EMAIL_HOST not found');
 
 export default async (email:string, magicLink:string) => {
   const transporter = nodemailer.createTransport({
     host,
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port,
+    secure, // true for 465, false for other ports
     auth: { 
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS }
+      user,
+      pass 
+    }
   });
 
   await transporter.sendMail({
     from: '"Gustavo Lendimuth" <gustavolendimuth@gmail.com>', // sender address
     to: email, // list of receivers
     subject: "Login", // Subject line
-    text: `link de login http://localhost:3000/login/${email}/${magicLink}`, // plain text body
-    html: `<p>Acesse o link abaixo para fazer o login</p><a href="http://localhost:3000/login/${email}/${magicLink}">http://localhost:3000/login/${email}/${magicLink}</a>`, // html body
+    text: `link de login ${process.env.FRONTEND_URL}/login/${email}/${magicLink}`, // plain text body
+    html: `<p>Acesse o link abaixo para fazer o login</p><a href="${process.env.FRONTEND_URL}/login/${email}/${magicLink}">${process.env.FRONTEND_URL}/login/${email}/${magicLink}</a>`, // html body
   });
 
   // console.log("Message sent: %s", info.messageId);
