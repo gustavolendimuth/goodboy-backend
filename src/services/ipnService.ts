51c5bee9a6fbcd86c9cc13a7311cd836b39756c9
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-02-21 22:30:13 -0300

    fix webhook and ipn logs

--- src/services/ipnService.ts
+++ src/services/ipnService.ts
@@ -1,30 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/prefer-default-export */
+import errorLog from '../utils/errorLog';
 import { fetchOrder, fetchPayment } from '../utils/fetchMercadoPago';
+import HttpException from '../utils/httpException';
 import { createOrderData } from '../utils/processPaymentUtils';
-import testeLog from '../utils/testeLog';
 import { createOrder, updateOrder } from './orderService';
 
 export const ipn = async (id:string, topic:string) => {
-  testeLog(topic);
+  console.log(topic, id);
 
-  if (topic === 'payment') {
-    const response = await fetchPayment.get(id);
+  try {
+    if (topic === 'payment') {
+      const response = await fetchPayment.get(id);
 
-    const result = await createOrderData({ order: response.data, email: response?.data?.payer?.email });
-    const order = await updateOrder({ data: result, paymentId: response.data.id });
-    testeLog(order);
-    return { message: 'order updated' };
+      const result = await createOrderData({ order: response.data, email: response?.data?.payer?.email });
+      const order = await updateOrder({ data: result, paymentId: response.data.id });
+      console.log('payment', response, result, order);
+      return { message: 'order updated' };
+    }
+
+    if (topic === 'merchant_order') {
+      const response = await fetchOrder.get(id);
+
+      const payment = await fetchPayment.get(response.data.payments[0].id);
+      const result = await createOrderData({ order: payment.data, email: payment?.data?.payer?.email });
+      console.log(response, payment, result);
+      await createOrder(result);
+      return { message: 'order created' };
+    }
+
+    return { message: 'nothing to update' };
+  } catch (error:any) {
+    errorLog(error);
+    throw new HttpException(400, 'Erro ao atualizar pedido');
   }
-
-  if (topic === 'merchant_order') {
-    const response = await fetchOrder.get(id);
-
-    const payment = await fetchPayment.get(response.data.payments[0].id);
-    const result = await createOrderData({ order: payment.data, email: payment?.data?.payer?.email });
-    testeLog(result);
-    await createOrder(result);
-    return { message: 'order created' };
-  }
-
-  return { message: 'nothing to update' };
 };
--- src/services/webhookService.ts
+++ src/services/webhookService.ts
@@ -1,21 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/prefer-default-export */
 import { WebhookBody } from '../interfaces';
+import errorLog from '../utils/errorLog';
 import { fetchPayment } from '../utils/fetchMercadoPago';
+import HttpException from '../utils/httpException';
 import { createOrderData } from '../utils/processPaymentUtils';
-import testeLog from '../utils/testeLog';
 import { updateOrder } from './orderService';
 
 export const webhook = async (body:WebhookBody) => {
-  testeLog(body);
+  try {
+    if (body.action === 'payment.updated') {
+      const response = await fetchPayment.get(body.data.id);
 
-  if (body.action === 'payment.updated') {
-    const response = await fetchPayment.get(body.data.id);
+      const result = await createOrderData({ order: response.data, email: response?.data?.payer?.email });
+      await updateOrder({ data: result, paymentId: response.data.id });
+      return { message: 'order updated' };
+    }
 
-    const result = await createOrderData({ order: response.data, email: response?.data?.payer?.email });
-    testeLog(result);
-    await updateOrder({ data: result, paymentId: response.data.id });
-    return { message: 'order updated' };
+    return { message: 'nothing to update' };
+  } catch (error:any) {
+    errorLog(error);
+    throw new HttpException(400, 'Erro ao atualizar pedido');
   }
-
-  return { message: 'nothing to update' };
 };
