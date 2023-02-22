912ce670c29ab9651d349a89c27f00b338c0c58b
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-02-21 23:04:51 -0300

    fix fetchMercadoPago dotenv

--- src/utils/fetchMercadoPago.ts
+++ src/utils/fetchMercadoPago.ts
@@ -4,10 +4,10 @@ import 'dotenv/config';
 
 export const fetchPayment = axios.create({
   baseURL: 'https://api.mercadopago.com/v1/payments/',
-  headers: { Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}` },
+  headers: { Authorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}` },
 });
 
 export const fetchOrder = axios.create({
   baseURL: 'https://api.mercadopago.com/merchant_orders/',
-  headers: { Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}` },
+  headers: { Authorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}` },
 });
