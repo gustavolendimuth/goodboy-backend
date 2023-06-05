b536ef808ba413fe544b65fa9b7fde743aba47cf
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-06-05 17:10:25 -0300

    fix: webhook controller return 200 status

--- src/controllers/ipnController.ts
+++ src/controllers/ipnController.ts
@@ -5,5 +5,5 @@ import { ipnService } from '../services/ipnService';
 export const ipnController = async (req: Request, res:Response) => {
   const { query: { id: paymentId, topic } } = req as never;
   await ipnService(paymentId, topic);
-  res.status(200);
+  return res.status(200);
 };
